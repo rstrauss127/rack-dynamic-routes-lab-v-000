e0f6aeed62538a408e7c7ab1704cae8702d96b07
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-29 00:14:55 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,5 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
+
+    if req.path == "/items"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
   end
 end
