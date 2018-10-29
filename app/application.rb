5d3ddf5a1345c32358862c55a59aeeb3d0c793bc
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-29 00:16:35 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,23 @@
+class Application
+
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+
+    if req.path.match(/items/)
+      item = req.path.split("/items/").last
+      if item_object = @@items.detect {|i| i.name == item}
+        resp.write "#{item_object.name}: $#{item_object.price}"
+      else
+        resp.write "Item not found"
+        resp.status = 400
+      end
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
+
+end
